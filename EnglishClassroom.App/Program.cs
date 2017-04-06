102d5e303983dec50ac2d7aca603e3e2299b4e68
Bryan Cua <bryancu4@gmail.com>
2017-04-06 03:27:26 +0100

    Add console application

---
+++ EnglishClassroom.App/Program.cs
@@ -0,0 +1,40 @@
+﻿using EnglishClassroom.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishClassroom.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("English Classroom");
+            Console.WriteLine("Morse code to English text and English text to Morse code.");
+            Console.WriteLine();
+
+            var translator = new Translator(new List<ILinguist> {
+                new EnglishToMorseCode(),
+                new MorseCodeToEnglish() });
+
+            var exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Enter either Morse Code or English");
+
+                var textToTranslate = Console.ReadLine();
+                if (textToTranslate.Equals("-q", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    exit = true;
+                }
+                else
+                {
+                    Console.WriteLine(translator.Translate(textToTranslate));
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
--- EnglishClassroom.App/Properties/AssemblyInfo.cs
+++ EnglishClassroom.App/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EnglishClassroom.Console")]
+[assembly: AssemblyTitle("EnglishClassroom.App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EnglishClassroom.Console")]
+[assembly: AssemblyProduct("EnglishClassroom.App")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("163f4c95-bd1f-4d65-8156-96247777a81e")]
+[assembly: Guid("35cffaf0-e93f-4018-9145-bbe71566f8e4")]
 
 // Version information for an assembly consists of the following four values:
 //
